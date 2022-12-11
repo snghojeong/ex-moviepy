c514e75845e6034b134fac60c74d8a566c1dc5c7
snghojeong <snghojeong@gmail.com>
2022-12-11 22:00:22 +0900

    Implement composite clip

--- test-vid-gen.py
+++ test-vid-gen.py
@@ -1,20 +1,8 @@
-# Import everything needed to edit video clips
-from moviepy.editor import *
-
-# Load myHolidays.mp4 and select the subclip 00:00:50 - 00:00:60
-clip = VideoFileClip("test_vid_src_01.mp4").subclip(5,10)
-
-# Reduce the audio volume (volume x 0.8)
-clip = clip.volumex(0.8)
-
-# Generate a text clip. You can customize the font, color, etc.
-txt_clip = TextClip("My Holidays 2013",fontsize=70,color='white')
-
-# Say that you want it to appear 10s at the center of the screen
-txt_clip = txt_clip.set_pos('center').set_duration(10)
-
-# Overlay the text clip on the first video clip
-video = CompositeVideoClip([clip, txt_clip])
-
-# Write the result to a file (many options available !)
-video.write_videofile("myHolidays_edited.webm")
+from moviepy.editor import VideoFileClip, clips_array, vfx
+clip1 = VideoFileClip("test_vid_src_01.mp4").margin(10).resize(width=640, height=360) # add 10px contour
+clip2 = clip1.fx( vfx.mirror_x)
+clip3 = clip1.fx( vfx.mirror_y)
+clip4 = clip1.resize(0.60) # downsize 60%
+final_clip = clips_array([[clip1, clip2],
+                          [clip3, clip4]])
+final_clip.write_videofile("my_stack.mp4")
