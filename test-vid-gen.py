890b85e9aabc8002a1b1221307d0322f50daf06e
Seungho Jeong <snghojeong@gmail.com>
2022-12-14 21:08:29 +0900

    Update test-vid-gen.py

--- test-vid-gen.py
+++ test-vid-gen.py
@@ -2,7 +2,6 @@ from moviepy.editor import VideoFileClip, clips_array, vfx
 clip1 = VideoFileClip("test_vid_src_01.mp4").margin(10).resize(width=640, height=360) # add 10px contour
 clip2 = clip1.fx( vfx.mirror_x)
 clip3 = clip1.fx( vfx.mirror_y)
-clip4 = clip1.resize(0.60) # downsize 60%
 final_clip = clips_array([[clip1, clip2],
-                          [clip3, clip4]])
+                          [clip3]])
 final_clip.write_videofile("my_stack.mp4")
