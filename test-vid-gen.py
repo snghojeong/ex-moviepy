481270a8c2b351b7e072060c250b0aea6fcc88d3
snghojeong <snghojeong@gmail.com>
2022-12-18 22:48:15 +0900

    Move QRcode location

--- test-vid-gen.py
+++ test-vid-gen.py
@@ -3,7 +3,10 @@ import qrcode
 from PIL import Image
 from moviepy.editor import VideoFileClip, clips_array, vfx
 
-clip1 = VideoFileClip("test_vid_src_01.mp4").resize(width=640, height=360)
+out_width = 640
+out_height = 360
+
+clip1 = VideoFileClip("test_vid_src_01.mp4").resize(width=out_width, height=out_height)
 def scroll(get_frame, t):
     QRcode = qrcode.QRCode(
         error_correction=qrcode.constants.ERROR_CORRECT_H
@@ -15,7 +18,7 @@ def scroll(get_frame, t):
         fill_color=QRcolor, back_color="white").resize((64, 64), Image.NEAREST).convert('RGB')
     qrimg_arr = numpy.asarray(QRimg);
     frame = get_frame(t)
-    frame[640-64:640,360-64:360,:] = qrimg_arr[:,:,:]
+    frame[out_height-64-32:out_height-32,out_width-64-32:out_width-32,:] = qrimg_arr[:,:,:]
     return frame
 modifiedClip = clip1.fl( scroll )
 modifiedClip.preview()
